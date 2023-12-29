5a33c2d7322e934ac919adb6d5e21c9c90619947
yuki sugawara <yuki.sugawara.be@gmail.com>
2023-12-29 23:56:54 +0900

    wantedlyとthreadsのアイコンを追加

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './style.css'
 // import viteLogo from '/vite.svg'
 import githubLogo from './github-mark.svg'
 import linkedLogo from './LI-In-Bug.png'
+import wantedlyLogo from './Wantedly_Mark_LightBG.svg'
+import threadsLogo from './threads-logo-black.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -10,8 +12,14 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://github.com/yuki-sg" target="_blank">
       <img src="${githubLogo}" class="logo" alt="GitHub logo" />
     </a>
+    <a href="https://www.wantedly.com/id/be__emo" target="_blank">
+      <img src="${wantedlyLogo}" class="logo" alt="Wantedly" />
+    </a>
     <a href="https://www.linkedin.com/in/yuki-sugawara-7821651ba/" target="_blank">
       <img src="${linkedLogo}" class="logo vanilla" alt="Linkedin logo" />
+    </a>
+    <a href="https://www.threads.net/@yk_sgwr" target="_blank">
+      <img src="${threadsLogo}" class="logo vanilla" alt="Threads logo" />
     </a>
     <h1>yuki sugar</h1>
     <div class="card">
